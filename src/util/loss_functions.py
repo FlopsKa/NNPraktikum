fix error computation

src/util/loss_functions.py
@@ -122,8 +122,8 @@ class CrossEntropyError(Error):
         self.error_string = 'crossentropy'
 
     def calculateError(self, target, output):
-        return - np.mean(target*np.log(output) + (1-target)*(np.log(1-output)))
+        return - target*np.log(output) + (1-target)*(np.log(1-output))
         
     def calculateDerivative(self, target, output):
         # factor x is missing
-        return np.mean(output - target)
+        return output - target