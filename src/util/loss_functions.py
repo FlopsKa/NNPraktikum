4cc1f7bee9eab2f3487abf3cb7451bc4f3fdfa53
gauchm <martin.gauch@student.kit.edu>
2018-06-16 10:42:52 +0200

    Back to right CE error

--- src/util/loss_functions.py
+++ src/util/loss_functions.py
@@ -122,7 +122,7 @@ class CrossEntropyError(Error):
         self.error_string = 'crossentropy'
 
     def calculateError(self, target, output):
-        return np.sum(target*np.log(output) + (1-target)*(np.log(1-output)))
+        return -1 * (np.sum(target*np.log(output) + (1-target)*(np.log(1-output))))
         
     def calculateDerivative(self, target, output):
         return -target / output + (1 - target) / (1 - output)
